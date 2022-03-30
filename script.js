43d7ab45e90eb40dcf3ec221e616d8d2d6627a02
SidharthVerma-tech <sidharthvermaiiits@gmail.com>
2022-03-30 10:00:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("Welcome to Tic Tac Toe");
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+//music.play();
+const changeTurn = ()=>{
+    return turn === "X"?"O": "X"
+}
+//Function to check for a Win
+const checkWin = () =>{
+    let boxtext=document.getElementsByClassName('boxtext');
+
+    let wins=[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText!=='')){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText+" Won";
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px";
+        }
+    })
+}
+//Main Logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(e =>{
+    let boxtext = e.querySelector('.boxtext');
+    e.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            document.getElementsByClassName(turn)[0].innerText = "Turn for " + turn;
+
+        }
+    })
+})
+reset.addEventListener('click',()=>{
+    let boxtexts=document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(e=>{
+        e.innerText="";
+    })
+})
